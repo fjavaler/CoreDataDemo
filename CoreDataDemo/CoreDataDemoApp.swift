ab85e05c2ea357d5e7e558c2e37cff7996fc7ebd
Frederick Javalera <fjavalera@goldpointsystems.com>
2021-09-14 16:47:58 -0600

    Initial Commit

---
+++ CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by Frederick Javalera on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
